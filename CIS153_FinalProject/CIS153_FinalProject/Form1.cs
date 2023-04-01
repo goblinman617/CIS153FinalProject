36c7f1a534cbc3eedc33e2cf586498f2e11e291c
goblinman617 <114950577+goblinman617@users.noreply.github.com>
2023-04-01 11:03:42 -0400

    Added name to code - BH

--- CIS153_FinalProject/CIS153_FinalProject/Form1.cs
+++ CIS153_FinalProject/CIS153_FinalProject/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CIS153_FinalProject {
+    //Brad Hanson
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
