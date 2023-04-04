8a639fb40df17083960324c29f967213756bc5db
Ben <benstreasurebox@gmail.com>
2023-04-04 18:51:51 -0400

    feat: window controller

--- CIS153_FinalProject/CIS153_FinalProject/TEST_FORM.Designer.cs
+++ CIS153_FinalProject/CIS153_FinalProject/TEST_FORM.Designer.cs
@@ -1,5 +1,7 @@
-﻿namespace CIS153_FinalProject {
-    partial class Form1 {
+﻿namespace CIS153_FinalProject
+{
+    partial class TEST_FORM
+    {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -9,8 +11,10 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing) {
-            if (disposing && (components != null)) {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -22,35 +26,35 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
-            this.button1 = new System.Windows.Forms.Button();
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(265, 177);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(236, 63);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Test Window";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TEST_FORM";
             // 
-            // Form1
+            // TEST_FORM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Name = "TEST_FORM";
+            this.Text = "TEST_FORM";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
-
