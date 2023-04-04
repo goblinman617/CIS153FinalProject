97ba450571fbb064451e789a234994f0caa6f016
Ben <benstreasurebox@gmail.com>
2023-04-04 19:09:11 -0400

    feat: basic concept for player controller.

---
+++ CIS153_FinalProject/CIS153_FinalProject/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS153_FinalProject
+{
+    internal class Player
+    {
+        private string name;
+
+        // Custom color for the checkers would be cool.
+        private Color color = Color.Red;
+
+        /* Getters */
+        public string getName()
+        { return name; }
+
+        /* Setters */
+        public void setName(string _name)
+        { name = _name; }
+    }
+}
